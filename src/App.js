6c184d81c289460693baab6521c4793d24c7792f
ikoote1 <ikootepreim@gmail.com>
2023-03-22 00:34:13 +0300

    Updated the project basing on the requirements and suggestions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Calculator from './components/Calculator';
-// import Screen from './components/Screen';
 
 function App() {
   return (
     <div className="App">
-      {/* <Screen /> */}
       <Calculator />
     </div>
   );
