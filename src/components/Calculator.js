7a8ea117c91cdc3e18d2bcc65e0fee71d556a5c0
ikoote1 <ikootepreim@gmail.com>
2023-03-21 22:44:44 +0300

    Started to add styling to the button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,24 +1,29 @@
 export default function Calculator() {
-  <div className="calculate">
-    <div className="screen">0</div>
-    <button type="button" className="btn">AC</button>
-    <button type="button" className="btn">+/-</button>
-    <button type="button" className="btn">%</button>
-    <button type="button" className="btn">/</button>
-    <button type="button" className="btn">7</button>
-    <button type="button" className="btn">8</button>
-    <button type="button" className="btn">9</button>
-    <button type="button" className="btn">*</button>
-    <button type="button" className="btn">4</button>
-    <button type="button" className="btn">5</button>
-    <button type="button" className="btn">6</button>
-    <button type="button" className="btn">-</button>
-    <button type="button" className="btn">1</button>
-    <button type="button" className="btn">2</button>
-    <button type="button" className="btn">3</button>
-    <button type="button" className="btn">+</button>
-    <button type="button" className="btn">0</button>
-    <button type="button" className="btn">.</button>
-    <button type="button" className="btn">=</button>
-  </div>;
+  return (
+    <div className="calculate">
+      <div className="screen">0</div>
+      <div className="btns">
+        <button type="button" className="btn">AC</button>
+        <button type="button" className="btn">+/-</button>
+        <button type="button" className="btn">%</button>
+        <button type="button" className="btn btn1">/</button>
+        <button type="button" className="btn">7</button>
+        <button type="button" className="btn">8</button>
+        <button type="button" className="btn">9</button>
+        <button type="button" className="btn btn1">*</button>
+        <button type="button" className="btn">4</button>
+        <button type="button" className="btn">5</button>
+        <button type="button" className="btn">6</button>
+        <button type="button" className="btn btn1">-</button>
+        <button type="button" className="btn">1</button>
+        <button type="button" className="btn">2</button>
+        <button type="button" className="btn">3</button>
+        <button type="button" className="btn btn1">+</button>
+        <button type="button" className="btn zero">0</button>
+        <button type="button" className="btn">.</button>
+        <button type="button" className="btn btn1">=</button>
+      </div>
+
+    </div>
+  );
 }
