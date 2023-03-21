abccd315d46058d4ac70d81a495fe6127cec753d
ikoote1 <ikootepreim@gmail.com>
2023-03-21 21:14:57 +0300

    Created files for the code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,24 @@
-function calculator (){
-    
+export default function Calculator() {
+  <div className="calculate">
+    <div className="screen">0</div>
+    <button type="button" className="btn">AC</button>
+    <button type="button" className="btn">+/-</button>
+    <button type="button" className="btn">%</button>
+    <button type="button" className="btn">/</button>
+    <button type="button" className="btn">7</button>
+    <button type="button" className="btn">8</button>
+    <button type="button" className="btn">9</button>
+    <button type="button" className="btn">*</button>
+    <button type="button" className="btn">4</button>
+    <button type="button" className="btn">5</button>
+    <button type="button" className="btn">6</button>
+    <button type="button" className="btn">-</button>
+    <button type="button" className="btn">1</button>
+    <button type="button" className="btn">2</button>
+    <button type="button" className="btn">3</button>
+    <button type="button" className="btn">+</button>
+    <button type="button" className="btn">0</button>
+    <button type="button" className="btn">.</button>
+    <button type="button" className="btn">=</button>
+  </div>;
 }
