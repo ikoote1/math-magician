d7318d1c8931e84d30793891c770c48a08c72684
ikoote1 <ikootepreim@gmail.com>
2023-03-23 11:39:56 +0300

    Fixed some linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,42 @@
+import React, { useState } from 'react';
 import Screen from './Screen';
-import React, {useState} from 'react';
 import calculate from '../logic/calculate';
 
 export default function Calculator() {
   const [state, setState] = useState({
-    total : null,
-    next : null,
-    operation: null
+    total: null,
+    next: null,
+    operation: null,
   });
 
-  const preimClick = (buttonName) =>{
+  const preimClick = (buttonName) => {
     const newState = calculate(state, buttonName);
     setState(newState);
-  }
-
+  };
 
   return (
     <div className="calculate">
       <Screen />
       <div className="btns">
-        <button type="button" className="btn" onClick={()=>preimClick('AC')}>AC</button>
-        <button type="button" className="btn" onClick={()=>preimClick('+/-')}>+/-</button>
-        <button type="button" className="btn" onClick={()=>preimClick('%')}>%</button>
-        <button type="button" className="btn btn1" onClick={()=>preimClick('/')}>/</button>
-        <button type="button" className="btn" onClick={()=>preimClick('7')}>7</button>
-        <button type="button" className="btn" onClick={()=>preimClick('8')}>8</button>
-        <button type="button" className="btn" onClick={()=>preimClick('9')}>9</button>
-        <button type="button" className="btn btn1" onClick={()=>preimClick('*')}>*</button>
-        <button type="button" className="btn" onClick={()=>preimClick('4')}>4</button>
-        <button type="button" className="btn" onClick={()=>preimClick('5')}>5</button>
-        <button type="button" className="btn" onClick={()=>preimClick('6')}>6</button>
-        <button type="button" className="btn btn1" onClick={()=>preimClick('-')}>-</button>
-        <button type="button" className="btn" onClick={()=>preimClick('1')}>1</button>
-        <button type="button" className="btn" onClick={()=>preimClick('2')}>2</button>
-        <button type="button" className="btn" onClick={()=>preimClick('3')}>3</button>
-        <button type="button" className="btn btn1" onClick={()=>preimClick('+')}>+</button>
-        <button type="button" className="btn zero" onClick={()=>preimClick('0')}>0</button>
-        <button type="button" className="btn" onClick={()=>preimClick('.')}>.</button>
-        <button type="button" className="btn btn1" onClick={()=>preimClick('=')}>=</button>
+        <button type="button" className="btn" onClick={() => preimClick('AC')}>AC</button>
+        <button type="button" className="btn" onClick={() => preimClick('+/-')}>+/-</button>
+        <button type="button" className="btn" onClick={() => preimClick('%')}>%</button>
+        <button type="button" className="btn btn1" onClick={() => preimClick('/')}>/</button>
+        <button type="button" className="btn" onClick={() => preimClick('7')}>7</button>
+        <button type="button" className="btn" onClick={() => preimClick('8')}>8</button>
+        <button type="button" className="btn" onClick={() => preimClick('9')}>9</button>
+        <button type="button" className="btn btn1" onClick={() => preimClick('*')}>*</button>
+        <button type="button" className="btn" onClick={() => preimClick('4')}>4</button>
+        <button type="button" className="btn" onClick={() => preimClick('5')}>5</button>
+        <button type="button" className="btn" onClick={() => preimClick('6')}>6</button>
+        <button type="button" className="btn btn1" onClick={() => preimClick('-')}>-</button>
+        <button type="button" className="btn" onClick={() => preimClick('1')}>1</button>
+        <button type="button" className="btn" onClick={() => preimClick('2')}>2</button>
+        <button type="button" className="btn" onClick={() => preimClick('3')}>3</button>
+        <button type="button" className="btn btn1" onClick={() => preimClick('+')}>+</button>
+        <button type="button" className="btn zero" onClick={() => preimClick('0')}>0</button>
+        <button type="button" className="btn" onClick={() => preimClick('.')}>.</button>
+        <button type="button" className="btn btn1" onClick={() => preimClick('=')}>=</button>
       </div>
 
     </div>
