559dab25e2fd01a3eba1ed20ea78deac9e710192
ikoote1 <ikootepreim@gmail.com>
2023-03-23 15:41:58 +0300

    Created new components for displaying quotes

---
+++ src/components/DisplayQuates.js
@@ -0,0 +1,36 @@
+
+
+function App() {
+  const [data, setData] = useState([])
+  const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+        const json = await res.json()
+        setData(json)
+      } catch (error) {
+        setHasError(true)
+      }
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [setData, setIsLoading])
+
+  if (hasError) return <div>Something went wrong!</div>
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <ul>
+      {data.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default App
